01bb493cd0f4b54f9d355c334e456259a1ef2cee
Akos-1 <veronicagyasi@gmail.com>
2023-04-24 05:24:51 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,16 +2,17 @@
 /**
  * main - Entry point
  * Description: 'three possible number formations'
+ * Return: always 0
  */
 int main(void)
 {
 	int x, y, z;
 
-	for (x = 48, x < 58; x++)
+	for (x = 48; x < 58; x++)
 	{
-		for (y = 49, y < 58; y++)
+		for (y = 49; y < 58; y++)
 		{
-			for (z = 50, z < 58; z++)
+			for (z = 50; z < 58; z++)
 			{
 				if (z > y && y > x)
 				{
@@ -19,14 +20,13 @@ int main(void)
 					putchar(y);
 					putchar(z);
 					if (x != 55 || y != 56)
-					{
-						putchar(',');
-						putchar(' ');
+						continue;
+					putchar(',');
+					putchar(' ');
 					}
 				}
 			}
 		}
-	}
 	putchar('\n');
 	return (0);
 }
