a1771814ef77ef4102ba545b8c724adbb711c28c
Akos-1 <veronicagyasi@gmail.com>
2023-04-25 15:07:22 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets 10 times in lowercase
+ *
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	for (b = a; a <= 10; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
