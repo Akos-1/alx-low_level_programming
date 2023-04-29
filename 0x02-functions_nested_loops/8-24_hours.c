25b8b39815a863c954f301238fea2072d332a410
Akos-1 <veronicagyasi@gmail.com>
2023-04-29 11:34:15 -0700

    void jack_bauer(void)

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,43 +1,30 @@
 #include "main.h"
 /**
- * jack_bauer - every minutes in a day
- *
- * Return: 0 if successful
+ * jack_bauer - every minute of the day
+ * Return: 0
  */
 void jack_bauer(void)
 {
-	int a, b;
+	int a = 0;
+	int b = 0;
+	int c;
+	int d;
 
-	for (a = 0; a < 24; a++)
+	while (a <= 23)
 	{
-		for (b = 0; b < 60; b++)
+		while (b <= 59)
 		{
-			if (a < 10)
-			{
-				_putchar(0);
-				_putchar(a + 0);
-			}
-			else
-			{
-				_putchar(a / 10 + 0);
-				_putchar(a % 10 + 0);
-			}
+			d = b % 10;
+			c = a % 10;
+			_putchar(a / 10 + 0);
+			_putchar(c + 0);
 			_putchar(':');
-			if (b < 10)
-			{
-				_putchar(0);
-				_putchar(b + 0);
-			}
-			else
-			{
-				_putchar(b / 10 + 0);
-				_putchar(b % 10 + 0);
-			}
+			_putchar(b / 10 + 0);
+			_putchar(d + 0);
+			b++;
 			_putchar('\n');
-			if (a == 23 && b == 29)
-			{
-				return;
-			}
 		}
+		a++;
+		b = 0;
 	}
 }
