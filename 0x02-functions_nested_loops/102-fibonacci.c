e3136a8fd9ad51d88fdc5ef7c6af4b5ab3545921
Akos-1 <veronicagyasi@gmail.com>
2023-04-29 11:11:42 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - sum of the Fibonacci numbers
+ * Return: always (0)
+ */
+int main(void)
+{
+	int c;
+	long int x, y, z;
+
+	x = 1;
+	y = 2;
+	printf("%ld,%ld", x, y);
+	for (c = 0; c < 48; c++)
+	{
+		z = x + y;
+		printf(",%ld", z);
+		x = y;
+		y = z;
+	}
+	printf('\n');
+	return (0);
+}
