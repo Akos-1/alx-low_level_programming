461ceb996e5bef8b018fcd77ed65a225a5700fba
Akos-1 <veronicagyasi@gmail.com>
2023-04-30 15:47:56 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - natural numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a, sum = o;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			sum += a;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
