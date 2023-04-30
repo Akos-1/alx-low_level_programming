b3322eb35c7e69a9a553512e54c24c72bd97a657
Akos-1 <veronicagyasi@gmail.com>
2023-04-30 08:59:19 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - straight line and times
+ * @n: the number of times straight line is printed
+ * Return: no return
+ */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
