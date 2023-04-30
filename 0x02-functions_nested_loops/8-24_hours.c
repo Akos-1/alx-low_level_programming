b0af727d07fe079511abad9a103a95ca903920bb
Akos-1 <veronicagyasi@gmail.com>
2023-04-30 09:10:24 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,30 +1,21 @@
 #include "main.h"
 /**
- * jack_bauer - every minute of the day
- * Return: 0
+ * jack_bauer - every minutes of the day
  */
 void jack_bauer(void)
 {
-	int a = 0;
-	int b = 0;
-	int c;
-	int d;
+	int x, y;
 
-	while (a <= 23)
+	for (x = 0; x <= 23; x++)
 	{
-		while (b <= 59)
+		for (y = 0; y <= 59; y++)
 		{
-			d = b % 10;
-			c = a % 10;
-			_putchar(a / 10 + 0);
-			_putchar(c + 0);
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
 			_putchar(':');
-			_putchar(b / 10 + 0);
-			_putchar(d + 0);
-			b++;
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
 			_putchar('\n');
 		}
-		a++;
-		b = 0;
 	}
 }
