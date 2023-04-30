01de201d11a8f8ee17aafe0c9b4de33bd22ba159
Akos-1 <veronicagyasi@gmail.com>
2023-04-30 14:32:33 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,32 +5,21 @@
  */
 void times_table(void)
 {
-	int a, b, c, d, e;
+	int a, b, c;
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		_putchar('0');
+		for (b = 1; b <= 9; b++)
 		{
+			_putchar(',');
+			_putchar(' ');
 			c = a * b;
-			if (c > 9)
-			{
-				d = c % 10;
-				e = (c - d) / 10;
-				_putchar(44);
-				_putchar(32);
-				_putchar(e + 0);
-				_putchar(d + 0);
-			}
+			if (c <= 9)
+				_putchar(' ');
 			else
-			{
-				if (b != 0)
-				{
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
-				}
-				_putchar(c + 0);
-			}
+				_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
 	}
