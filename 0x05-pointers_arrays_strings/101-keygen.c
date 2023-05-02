6c1ee3d4abef70444bb0d3a575f54694ba9d0730
Akos-1 <veronicagyasi@gmail.com>
2023-05-02 16:17:03 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generates random vlid passwords
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int pass[100]
+	int a, b, c;
+
+	b = 0;
+	srand(time(NULL));
+	for (a = 0; a < 100; a++)
+	{
+		pass[a] = rand() % 78;
+		b += (pass[a] + '0');
+		putchar(pass[a] + '0');
+		if ((2772 - b) - '0' < 78)
+		{
+			c = 2772 - b - '0';
+			b += c;
+			putcahr(c + '0');
+			break;
+		}
+	}
+	return (0);
+}
+
