c50c9e2eacd5b04a59acfb56ad90ffcc0e5ef3df
Akos-1 <veronicagyasi@gmail.com>
2023-05-02 22:01:11 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,16 +8,13 @@
 char *_strcpy(char *dest, char *src)
 {
 	int a = 0;
-	int b = 0;
 
-	while (*(src + a) != '0\')
+	while (a >= 0)
 	{
+		*(dest + a) = *(src + a);
+		if (*(src + a) == '\0')
+			break;
 		a++;
 	}
-	for (; b < a ; b++)
-	{
-		dest[b] = src[b];
-	}
-	dest[a] = '0\';
 	return (dest);
 }
