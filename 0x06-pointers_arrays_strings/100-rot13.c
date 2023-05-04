05edf75e7bb1082e98937527aa3c1a89fd3f6cb0
Akos-1 <veronicagyasi@gmail.com>
2023-05-04 14:59:48 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - encodes a string using rot13
+ * @s: pointer to string
+ * Return: *s
+ */
+char *rot13(char *s)
+{
+	int x;
+	int y;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyxabcdefghijklm";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; y < 52; y++)
+		{
+			if (s[x] == data1[y])
+			{
+				s[x] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
