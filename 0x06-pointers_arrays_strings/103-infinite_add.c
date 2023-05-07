4171c7b070cafdbb60205d91dd8d167f09439f59
Akos-1 <veronicagyasi@gmail.com>
2023-05-07 10:44:34 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -4,51 +4,47 @@
  * infinite_add - adds two numbers stored
  * @n1: first number to be added
  * @n2: second number to be added
- * @r: result
+ * @r:  buffer that the function will use to store the result
  * @size_r: buffer size
- * Result: a pointer
+ * Result: a pointer to the result
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int a1 = 0, a2 = 0, a3, a4, a5, a6, a = 0;
+	int a1 = 0, a2 = 0, a3 = 0, a4 = 0, a5 = 0, a6 = 0, a7 = 0;
 
-	while (*(n1 + a1) != '\0')
-		a1++;
-	while (*(n2 + a2) != '\0')
+	while (*(n1 + a2) != '\0')
 		a2++;
-	if (a1 >= a2)
-		a4 = a1;
-	else
-		a4 = a2;
-	if (size_r <= a4 + 1)
+	while (*(n2 + a3) != '\0')
+		a3++;
+	a2--;
+	a3--;
+	if (a3 >= size_r || a2 >= size_r)
 		return (0);
-	r[a4 + 1] = '\0';
-	a1--, a2--, size_r--;
-	a5 = *(n1 + a1) - 48, a6 = *(n2 + a2) - 48;
-	while (a4 >= 0)
+	while (a3 >= 0 || a2 >= 0 || a1 == 1)
 	{
-		a3 = a5 + a6 + a;
-		if (a3 >= 10)
-			a = a3 / 10;
-		else
-			a = 0;
-		if (a3 > 0)
-			*(r + a4) = (a3 % 10) + 48;
-		else
-			*(r + a4) = '0';
-		if (a1 > 0)
-			a1--, a5 = *(n1 + a1) - 48;
-		else
+		if (a2 < 0)
 			a5 = 0;
-		if (a2 > 0)
-			a2--, a6 = *(n2 + a2) - 48;
 		else
+			a5 = *(n1 + a2) - '0';
+		if (a3 < 0)
 			a6 = 0;
-		a4--, size_r--;
+		else
+			a6 = *(n2 + a3) - '0';
+		a7 = a5 + a6 + a1;
+		if (a7 >= 10)
+			a1 = 1;
+		else
+			a1 = 0;
+		if (a4 >= (size_r - 1))
+			return (0);
+		*(r + a4) = (a7 % 10) + '0';
+		a4++;
+		a3--;
+		a2--;
 	}
-	if (*(r) == '0')
-		return (r + 1);
-	else
-		return (r);
+	if (a4 == size_r)
+		return (0);
+	*(r + a4) = '\0';
+	rev_string(r);
+	return (r);
 }
-
