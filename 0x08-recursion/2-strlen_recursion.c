5576fea2c848de5ba98ca98566761c471a54b020
Akos-1 <veronicagyasi@gmail.com>
2023-05-10 15:11:19 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - brings back a string's length
+ * @s: inputed string
+ * Return: string's length
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len + -_strlen_recursion(s + 1);
+	}
+	return (len);
+}
