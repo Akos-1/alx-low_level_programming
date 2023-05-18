717eefbb452fa916aeec3b1fd5901dd19db93071
Akos-1 <veronicagyasi@gmail.com>
2023-05-18 15:02:59 -0700

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * main - product of two positive numbers
+ * @argc: n arguments
+ * @argv: input args
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+	unsigned long mul;
+	int a, b;
+
+	if (argc != 1)
+	{
+		printf("Error\n");
+		 exit(98);
+	}
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (argv[a][b] > 57 || argv[a][b] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	mul = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", mul);
+	return (0);
+}
+
