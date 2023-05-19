7eefafbdc1129cda210800898ccf7a252af53a92
Akos-1 <veronicagyasi@gmail.com>
2023-05-18 21:31:54 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -48,7 +48,7 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, z = 0;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -78,11 +78,11 @@ int main(int argc, char *argv[])
 	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
-			z = 1;
-		if (z)
+			a = 1;
+		if (a)
 			_putchar(result[i] + '0');
 	}
-	if (!z)
+	if (!a)
 		_putchar('0');
 	_putchar('\n');
 	free(result);
