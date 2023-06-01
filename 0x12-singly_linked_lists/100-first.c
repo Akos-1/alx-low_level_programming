e93880d512f2512d9e70fdc95a99877c09c7cfc9
Akos-1 <veronicagyasi@gmail.com>
2023-06-01 15:55:16 -0700

    Update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,9 +2,9 @@
 /**
  * premain - prints a sentence before the main
  */
-void __attribute__((constructor)) premain()
+void __attribute__((constructor))premain()
 {
-	printf("You're beat! and yet, you must allow, \n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
 
