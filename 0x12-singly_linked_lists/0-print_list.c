f599080f094efe3e294682b7efcf9fbb7eddda8e
Akos-1 <veronicagyasi@gmail.com>
2023-06-01 12:50:49 -0700

    Print1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,12 +9,13 @@ size_t print_list(const list_t *h)
 {
 	size_t a = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		printf("[%d] %sn", h->len,
-				h->str == NULL ? "(nil)" : h->str);
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		a++;
-	}
-	return (a);
+	}	return (a);
 }
