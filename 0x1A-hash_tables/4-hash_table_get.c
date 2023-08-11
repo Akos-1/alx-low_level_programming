4c8e5250ecd122d7cd79f1287ea74149fdb45ed0
Akos-1 <veronicagyasi@gmail.com>
2023-08-10 22:12:51 -0700

    a function that retrieves a value associated with a key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - add an element to the hash table
+ * @ht: a pointer to the hash table
+ * @key: the key to be added
+ * Return: NULL if the key cannot be matched 
+ * Otherwise the value associated with the ket in ht
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+	{
+		return (NULL);
+	}
+
+	index = hash_djb2((const unsigned char *)key) % ht->size;
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return (current->value);
+		}
+		current = current->next;
+	}
+
+	return (NULL);  
+}
